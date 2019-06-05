c523c55775daf5b0ec58743d7513fae53d84381b
SUETAK <akasirotonbo08451@gmail.com>
2019-06-06 01:46:01 +0900

    first commit

---
+++ SwiftStart/SwiftStart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftStart
+//
+//  Created by 末廣拓也 on 2019/06/05.
+//  Copyright © 2019 Suetak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
